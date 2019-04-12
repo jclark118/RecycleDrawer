752e2f1ed424f2aad347bf3aa3771ff0cc70b618
Joe Clark <joclark@caci.com>
2019-04-12 09:35:09 -0600

    when clicking on a state, show list of cities in that state

---
+++ app/src/main/java/com/caci/recycledrawer/Location.java
@@ -0,0 +1,66 @@
+package com.caci.recycledrawer;
+
+/**
+ * A Location made up of:
+ * City
+ * Lat
+ * Lon
+ * Population
+ * State
+ */
+public class Location {
+    String city;
+    String state;
+    String lat;
+    String lon;
+    String population;
+
+    public Location(String state, String city){
+        this.state = state;
+        this.city = city;
+    }
+
+    /**
+     * Getters and setters
+     * @return
+     */
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+}
--- app/src/main/java/com/caci/recycledrawer/MainActivity.java
+++ app/src/main/java/com/caci/recycledrawer/MainActivity.java
@@ -7,7 +7,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,7 +14,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,26 +23,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Creates a RecyclerView of a list of locations and gives it a bottom drawer behavior
+ * Creates a RecyclerView of a list of states and gives it a bottom drawer behavior
  */
 public class MainActivity extends AppCompatActivity implements LocationAdapter.RecyclerViewClickListener {
 
     /**
      * List of location names
      */
-    private List<String> locations = new ArrayList<>();
+    private List<String> states = new ArrayList<>();
 
     /**
      * List of city names
      */
-    private List<String> cities = new ArrayList<>();
+    private Map<String, List<String>> cities = new HashMap<>();
 
     /**
-     * RecyclerView that will hold our locations
+     * RecyclerView that will hold our states
      */
     private RecyclerView locationRecycler;
 
@@ -63,6 +63,11 @@ public class MainActivity extends AppCompatActivity implements LocationAdapter.R
      */
     private FloatingActionButton fab;
 
+    /**
+     * List of location objects to be build from the json data
+     */
+    private List<Location> locations = new ArrayList<>();
+
 
 
 
@@ -78,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements LocationAdapter.R
         // Create floating action button
         createFab();
 
-        // Generate a list of locations
+        // Generate a list of states
         generateLocations();
 
         // Create the RecyclerView
@@ -87,8 +92,76 @@ public class MainActivity extends AppCompatActivity implements LocationAdapter.R
     }
 
 
+
+
     /**
-     * open the locations file and parse
+     * Create the recycler view containing our states
+     */
+    private void createRecyclerView(){
+        // Attach the xml
+        locationRecycler = (RecyclerView) findViewById(R.id.recycler_locations);
+
+        // Create a layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        locationRecycler.setLayoutManager(layoutManager);
+
+        // Create the adapter
+        locationAdapter = new LocationAdapter(states, this);
+        locationRecycler.setAdapter(locationAdapter);
+    }
+
+    /**
+     * On click listener - show which location is clicked
+     * @param position - the clicked location
+     */
+    @Override
+    public void onClick(int position) {
+        Context context = this;
+        String state = states.get(position);
+        removeAllLocations();
+        putCitiesInRecycler(state);
+
+//        Toast.makeText(context, "removing: " + String.valueOf(position), Toast.LENGTH_SHORT)
+//                .show();
+//        removeLocation(position);
+
+    }
+
+
+
+
+
+    /**
+     * Remove a location from our list, and remove it from the RecyclerView
+     * @param position - the item to remove
+     */
+    public void removeLocation(int position){
+        states.remove(position);
+        locationAdapter.notifyItemRemoved(position);
+    }
+
+    /**
+     * Remove all states from the list, and remove them from the RecyclerView
+     */
+    public void removeAllLocations(){
+        states.clear();
+        locationAdapter.clear();
+        locationAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Give the list of cities (by state) to the adapter and bind
+     */
+    public void putCitiesInRecycler(String state){
+        cityAdapter = new LocationAdapter(getCitiesByState(state), this);
+        locationRecycler.swapAdapter(cityAdapter, true);
+    }
+
+
+
+
+    /**
+     * open the cities file and parse
      */
     private String loadLocations(){
         String json = null;
@@ -107,90 +180,62 @@ public class MainActivity extends AppCompatActivity implements LocationAdapter.R
     }
 
     /**
-     * Populate our location list from the json data
+     * Populate our location, city, and state list from the json data
      */
     private void generateLocations(){
         try {
             JSONArray array = new JSONArray(loadLocations());
             for(int i=0; i<array.length(); i++){
                 JSONObject row = array.getJSONObject(i);
-                if(!locations.contains(row.get("state"))) {
-                    locations.add(row.get("state").toString());
-                    Collections.sort(locations);
+                String state = row.get("state").toString();
+                String city = row.get("city").toString();
+
+                // Add city and state basic names
+                if(!states.contains(state)) {
+                    states.add(state);
                 }
+                if(!cities.containsKey(state)){
+                    List<String> tempCity = new ArrayList<>();
+                    tempCity.add(city);
+                    cities.put(state, tempCity);
+                }else{
+                    cities.get(state).add(city);
+                }
+
+                // Add a new location object to our list
+                Location loc = new Location(state, city);
+                locations.add(loc);
             }
+            // Sort the list of states and cities alphebetically
+            sortStatesAndCities();
         }catch (JSONException e) {
             e.printStackTrace();
         }
-
-        generateCities();
     }
 
-    private void generateCities(){
-        cities = new ArrayList<>();
-        cities.add("Austin");
-        cities.add("Boston");
-        cities.add("Denver");
-        cities.add("Herndon");
-        cities.add("LA");
+
+    /**
+     * Get a list of all cities belonging to the given state
+     * @param state - the state to search for
+     * @return - list of cities
+     */
+    private List<String> getCitiesByState(String state){
+        List<String> cityList = new ArrayList<>();
+        if(cities.containsKey(state)){
+            cityList = cities.get(state);
+        }
+        return cityList;
     }
 
     /**
-     * Create the recycler view containing our locations
+     * Sort both the States list and all cities lists in the city hashmap
      */
-    private void createRecyclerView(){
-        // Attach the xml
-        locationRecycler = (RecyclerView) findViewById(R.id.recycler_locations);
-
-        // Create a layout manager
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        locationRecycler.setLayoutManager(layoutManager);
-
-        // Create the adapter
-        locationAdapter = new LocationAdapter(locations, this);
-        locationRecycler.setAdapter(locationAdapter);
+    public void sortStatesAndCities(){
+        Collections.sort(states);
+        for(List<String> tempCities : cities.values()){
+            Collections.sort(tempCities);
+        }
     }
-
-    /**
-     * On click listener - show which location is clicked
-     * @param position - the clicked location
-     */
-    @Override
-    public void onClick(int position) {
-        Context context = this;
-//        Toast.makeText(context, "removing: " + String.valueOf(position), Toast.LENGTH_SHORT)
-//                .show();
-        removeLocation(position);
-//        removeAllLocations();
-    }
-
-    /**
-     * Remove a location from our list, and remove it from the RecyclerView
-     * @param position - the item to remove
-     */
-    public void removeLocation(int position){
-        locations.remove(position);
-        locationAdapter.notifyItemRemoved(position);
-    }
-
-    /**
-     * Remove all locations from the list, and remove them from the RecyclerView
-     */
-    public void removeAllLocations(){
-        locations.clear();
-        locationAdapter.clear();
-        locationAdapter.notifyDataSetChanged();
-        putCitiesInRecycler();
-    }
-
-    /**
-     * Give the list of cities to the adapter and bind
-     */
-    public void putCitiesInRecycler(){
-        cityAdapter = new LocationAdapter(cities, this);
-        locationRecycler.swapAdapter(cityAdapter, true);
-    }
-
 
     /**
      * Set up the Floating action button
